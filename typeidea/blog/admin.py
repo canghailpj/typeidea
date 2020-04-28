fd4a7eb4b71a385a8574fc3f9f945e66a0ac42a9
liupeijiang <778376242@qq.com>
2020-04-28 17:21:11 +0800

    admin注册

--- typeidea/blog/admin.py
+++ typeidea/blog/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Category,Post,Tag
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name','status','owner','created_time','is_nav')
+    fields = ('name','status','is_nav','owner')
+
+    def save_model(self, request, obj, form, change):
+        obj.owner = request.user
+        # return super(CategoryAdmin,self).save_model(request,obj,form,change)
+        obj.save()
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name','owner','status','created_time')
+    fields = ('name','status')
+
+
+    def save_model(self, request, obj, form, change):
+        obj.owner = request.user
+        # return super(CategoryAdmin,self).save_model(request,obj,form,change)
+        obj.save()
+
+# admin.site.register(Category,CategoryAdmin)
+
