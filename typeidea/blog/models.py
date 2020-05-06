2ab12a29a9e54eaf024ea5a57c20bd677588df26
liupeijiang <778376242@qq.com>
2020-05-06 17:29:56 +0800

    完善数据查询逻辑

--- typeidea/blog/models.py
+++ typeidea/blog/models.py
@@ -22,6 +22,23 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    #返回分类并区分是否为导航分类
+    @classmethod
+    def get_navs(cls):
+        categories = cls.objects.filter(status=cls.STATUS_NORMAL)
+        nav_categories = []
+        normal_categories = []
+        for cate in categories:
+            if cate.is_nav:
+                nav_categories.append(cate)
+            else:
+                normal_categories.append(cate)
+
+        return {
+            'navs':nav_categories,
+            'categories':normal_categories,
+        }
+
 
 class Tag(models.Model):
     STATUS_NORMAL = 1
@@ -68,3 +85,35 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+    @staticmethod
+    def get_by_tag(tag_id):
+        try:
+            tag = Tag.objects.get(id=tag_id)
+        except Tag.DoesNotExit:
+            tag = None
+            post_list = []
+        else:
+            post_list = tag.post_set.filter(status=Post.STATUS_NORMAL).select_related('owner','category')
+
+
+        return post_list,tag
+
+    @staticmethod
+    def get_by_category(category_id):
+        try:
+            category = Category.objects.get(id=category_id)
+        except Category.DoesNotExit:
+            category = None
+            post_list = []
+        else:
+            post_list = Category.post_set.all(status=Post.STATUS_NORMAL).select_related('owner','category')
+
+        return post_list,category
+
+    @classmethod
+    def latest_posts(cls):
+        return cls.objects.filter(status=cls.STATUS_NORMAL)
+
+
